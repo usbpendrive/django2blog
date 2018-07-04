07b7cd58934fca39c46a66a5255c8a2ac4ab04cd
usbpendrive <ferry.fernando@gmail.com>
2018-07-04 10:36:03 +0700

    Blog views.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -24,6 +25,12 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
 
     class Meta:
         ordering = ('-publish', )
